4dbbdc8fe6b0fa4a89fc1409015d1b07859d7754
ravifposeur <ravifgayuhwicaksono@gmail.com>
2026-05-20 15:42:29 +0700

    fix: expire token

--- app/backend/app/core/security.py
+++ app/backend/app/core/security.py
@@ -22,7 +22,7 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        datetime.utcnow() + timedelta(minutes=720)
+        expire = datetime.utcnow() + timedelta(minutes=720)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
